5b439fbb23f2ccf51d2420e2c883299ae78e9c53
Jeremy Ratliff <jcratfish@gmail.com>
2014-09-03 14:01:02 -0400

    Added blank project

---
+++ src/Google Drive Monitor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Google_Drive_Monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
